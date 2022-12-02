aafcc581c1c1115431c899f557fa1f50693db2b2
Rishat Agzamov <rishat.se@gmail.com>
2022-12-02 14:22:41 -0500

    feat: implement cli env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import process from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    for (let key in process.env) {
+        if (/^RSS_/.test(key)) console.log(`${key}=${process.env[key]};`);
+    }
+
 };
 
 parseEnv();
