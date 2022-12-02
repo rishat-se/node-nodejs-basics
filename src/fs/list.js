5e12d82c115431711d4571dfa243d66e6e7d4091
Rishat Agzamov <rishat.se@gmail.com>
2022-12-02 12:52:48 -0500

    feat: implement fs list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import * as fs from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
-};
+    // Write your code here
+    const FOLDER = 'src/fs/files/';
+    try {
+        const files = await fs.readdir(FOLDER);
+        for (const fileName of files) console.log(fileName);
+    } catch {
+        throw Error('FS operation failed');
+    }
+}
 
 await list();
