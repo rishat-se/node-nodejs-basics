a667ba3eb1126d74be2a35d1bdae5851218c3dec
Rishat Agzamov <rishat.se@gmail.com>
2022-12-02 12:50:02 -0500

    feat: implement fs delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
+
 const remove = async () => {
     // Write your code here 
+    const FILE = 'src/fs/files/fileToRemove.txt';
+    try {
+        await fs.rm(FILE);
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
 await remove();
