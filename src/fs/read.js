6e38a65e801a24af25a6ac37ccc4f23a8d945bc5
Rishat Agzamov <rishat.se@gmail.com>
2022-12-02 13:02:08 -0500

    feat: implement fs read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import * as fs from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const FILE = 'src/fs/files/fileToRead.txt';
+    try {
+        let content = await fs.readFile(FILE, { encoding: 'utf8' });
+        console.log(content);
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
 await read();
