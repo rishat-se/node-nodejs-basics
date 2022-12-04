0a3a74f98d94ca44c82a3e631a7b2041f9ac14aa
Rishat Agzamov <rishat.se@gmail.com>
2022-12-04 12:31:57 -0500

    feat: implement zip compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const INPUT_FILE = 'src/zip/files/fileToCompress.txt';
+    const OUTPUT_FILE = 'src/zip/files/archive.gz';
+    try {
+        const readStream = createReadStream(INPUT_FILE);
+        const writeStream = createWriteStream(OUTPUT_FILE);
+        const gzip = createGzip();
+        await pipeline(
+            readStream,
+            gzip,
+            writeStream
+        )
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await compress();
