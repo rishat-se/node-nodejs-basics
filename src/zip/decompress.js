0306c30967c427fe7c4427f5cce54f52d922cb76
Rishat Agzamov <rishat.se@gmail.com>
2022-12-04 12:32:16 -0500

    feat: implement zip decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createUnzip } from 'node:zlib';
+
+
 const decompress = async () => {
-    // Write your code here 
+    const INPUT_FILE = 'src/zip/files/archive.gz';
+    const OUTPUT_FILE = 'src/zip/files/fileToCompress.txt';
+    try {
+        const readStream = createReadStream(INPUT_FILE);
+        const writeStream = createWriteStream(OUTPUT_FILE);
+        const unzip = createUnzip();
+        await pipeline(
+            readStream,
+            unzip,
+            writeStream
+        );
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 await decompress();
