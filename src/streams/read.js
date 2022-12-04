9fac7d10ea9c0adc91cc77f4d4d273b40de62cf5
Rishat Agzamov <rishat.se@gmail.com>
2022-12-04 11:44:07 -0500

    feat: implement stream read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import process from 'node:process';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const read = async () => {
-    // Write your code here 
+    const INPUT_FILE = 'src/streams/files/fileToRead.txt';
+    try {
+        const readStream = createReadStream(INPUT_FILE);
+        await pipeline(
+            readStream,
+            process.stdout
+        );
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await read();
