50e7b38fd8b3b13437d84fc04961926c89b2b1f3
Rishat Agzamov <rishat.se@gmail.com>
2022-12-04 11:40:08 -0500

    feat: implement hash calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createHash } from 'node:crypto';
+import { Transform } from 'node:stream';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const FILE = 'src/hash/files/fileToCalculateHashFor.txt';
+    try {
+        const readStream = createReadStream(FILE);
+        const hash = createHash('sha256');
+        const toHex = new Transform({
+            transform(chunk, encoding, callback) {
+                this.push(chunk.toString('hex'));
+                callback();
+            }
+        })
+        await pipeline(
+            readStream,
+            hash,
+            toHex,
+            process.stdout
+        )
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await calculateHash();
