ed0e130cb68f6b4f175a6635629eb9cdfc61dec0
Rishat Agzamov <rishat.se@gmail.com>
2022-12-04 11:47:40 -0500

    feat: implement streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import process from 'node:process';
+import { createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const write = async () => {
-    // Write your code here 
+    const OUTPUT_FILE = 'src/streams/files/fileToWrite.txt';
+    try {
+        const writeStream = createWriteStream(OUTPUT_FILE);
+        await pipeline(
+            process.stdin,
+            writeStream
+        )
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await write();
