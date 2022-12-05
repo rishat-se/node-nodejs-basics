4576f95be52a7886b295d7b9548f2c502c294a55
Rishat Agzamov <rishat.se@gmail.com>
2022-12-05 13:46:27 -0500

    feat: implement cp cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import { fork } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const SCRIPT = __dirname + '/files/script.js';
+    const child = fork(SCRIPT, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
